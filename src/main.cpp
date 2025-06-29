4e29c20f76460a47c6f20ec7b9605cf218064fd5
lune0808 <yagigami.goatgod@gmail.com>
2025-06-29 22:56:42 +0200

    hello triangle

--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,31 @@ int main()
 		"}\n\0"sv,
 	};
 	if (!shader.ok()) return 1;
+	shader.bind();
+
+	float vdata[] = {
+		-0.5f, -0.5f,
+		+0.5f, -0.5f,
+		 0.0f, +0.5f,
+	};
+	GLuint glbufs[2];
+	glGenVertexArrays(1, &glbufs[0]);
+	glBindVertexArray(glbufs[0]);
+	glGenBuffers(1, &glbufs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, glbufs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof vdata, vdata, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), nullptr);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
 
 	while (win.live()) {
+		glBindVertexArray(glbufs[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		win.draw();
 	}
 
+	glDeleteBuffers(1, &glbufs[1]);
+	glDeleteVertexArrays(1, &glbufs[0]);
 	shader.fini();
 }
 
